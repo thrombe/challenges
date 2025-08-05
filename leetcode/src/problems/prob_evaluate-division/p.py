73c68a0d4fe318037c2fe31ecc448c8786bfde93
thrombe <yash.n.thombare620@gmail.com>
2025-08-05 23:47:13 +0530

    leetcode evaluate-division try 1

---
+++ leetcode/src/problems/prob_evaluate-division/p.py
@@ -0,0 +1,82 @@
+from typing import *
+
+
+class Solution:
+    def calcEquation(
+        self, equations: List[List[str]], values: List[float], queries: List[List[str]]
+    ) -> List[float]:
+        import copy
+        import random
+
+        _subs = {}
+        defined_vars = set()
+        for (a, b), v in zip(equations, values):
+            defined_vars.add(a)
+            defined_vars.add(b)
+
+            if a not in _subs:
+                _subs[a] = []
+            if b not in _subs:
+                _subs[b] = []
+            _subs[a].append([v, b])
+            _subs[b].append([1 / v, a])
+
+        ans = []
+
+        def _solve_query(a, b, subs):
+            if a not in defined_vars:
+                ans.append(-1)
+                return
+            if b not in defined_vars:
+                ans.append(-1)
+                return
+
+            if a == b:
+                ans.append(1)
+                return
+
+            val = 1
+            var = a
+            print(f"\nsolving {a}/{b}")
+            while var != b:
+                if var not in subs:
+                    ans.append(-1)
+                    return
+
+                sub = subs[var]
+                subs[var] = sub[:-1] + sub[-1:]
+                if len(subs[var]) == 0:
+                    subs.pop(var)
+
+                v, s = sub[random.randrange(0, len(sub))]
+                print(f"{var} -> {v} * {s}")
+                val *= v
+                var = s
+                print(f"{var} * {val} = {b}")
+
+            ans.append(val)
+            return
+
+        for [a, b] in queries:
+            subs = copy.deepcopy(_subs)
+            _solve_query(a, b, subs)
+
+        return ans
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    ans = sol.calcEquation(
+        [["x1", "x2"], ["x2", "x3"], ["x3", "x4"], ["x4", "x5"]],
+        [3.0, 4.0, 5.0, 6.0],
+        [
+            ["x1", "x5"],
+            ["x5", "x2"],
+            ["x2", "x4"],
+            ["x2", "x2"],
+            ["x2", "x9"],
+            ["x9", "x9"],
+        ],
+    )
+
+    print(ans)
